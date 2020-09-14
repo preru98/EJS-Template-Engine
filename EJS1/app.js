1f849d32092d8aad26fc130161f835d582155cd4
Prerna <prerna1998.ps@gmail.com>
2020-09-14 11:27:36 +0530

    Passing varibles

--- EJS1/app.js
+++ EJS1/app.js
@@ -13,7 +13,28 @@ app.use(express.static('views'));                    //view engine setup
 app.set('view engine', 'ejs');                  
 
 app.get('/', (req, res) => {
-    res.render('pages/index');
+    let posts = [
+        {
+            title : "Water Bay",
+            author : "Pi Sharma",
+            description : "Lorem Ipsum Water ..."
+        },
+        {
+            title : "Sun Warm",
+            author : "Hihello",
+            description : "Lorem Ipsum Sun ..."
+        },
+        {
+            title : "Moon Docomo",
+            author : "Couver",
+            description : "Lorem Ipsum Moon ..."
+        }
+    ];
+    let tagline = "Admire nature to the fullest, being beautiful ...";
+    res.render('pages/index', {
+        posts : posts,
+        tagline : tagline
+    });
 });
 
 app.get('/about', (req, res) => {
